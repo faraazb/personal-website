4870e85272e22306a1650ead7cebaeaee424bc45
Faraaz Biyabani <faraazb@protonmail.com>
2024-02-17 03:02:05 +0530

    feat: sitemap and SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
 import preact from "@astrojs/preact";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact({ compat: true })],
+  integrations: [
+    preact({
+      compat: true,
+    }),
+    sitemap(),
+  ],
   site: "https://faraazbiyabani.me",
 });
